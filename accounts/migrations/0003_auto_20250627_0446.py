a5255512f3b052a76695b2b7eb8c40ced280b0ed
Abdul Ahad <abdulahad53688@gmail.com>
2025-07-01 11:05:17 +0500

    feat: Now tracking the migrations files as well.

---
+++ accounts/migrations/0003_auto_20250627_0446.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+def set_notes_for_existing_users(apps, schema_editor):
+    CustomUser = apps.get_model('accounts', 'CustomUser')
+    for user in CustomUser.objects.all():
+        if not user.notes:
+            user.notes = "Auto note for existing user"
+            user.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts',  '0002_customuser_notes'),
+    ]
+
+    operations = [
+        # Temporarily disable this to avoid issues while resetting/fixing migrations
+        # migrations.RunPython(set_notes_for_existing_users),
+    ]
